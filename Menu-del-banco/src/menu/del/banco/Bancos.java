74717233197a2a098c1fde7790ea5aac2f064c7b
Antonio <eazapatab@alumno.uspg.edu.gt>
2020-09-18 22:48:31 -0600

    Agregue dos clases Emmanuel Zapata y Josue Tejeda fallas tecnicas en su compu

---
+++ Menu-del-banco/src/menu/del/banco/Bancos.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu.del.banco;
+
+/**
+ *
+ * @author Emmanuel Zapata
+ */
+public class Bancos {
+    private String tipoCuenta;
+    private String nombre;
+    private int saldo;
+    private int id;
+       public Bancos(String tipoCuenta, String nombre, int saldo, int id) {
+        this.tipoCuenta = tipoCuenta;
+        this.nombre = nombre;
+        this.saldo = saldo;
+    this.id = id;
+    }
+   
+    public String getTipoCuenta() {
+        return tipoCuenta;
+    }
+   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipoCuenta(String tipo) {
+        this.tipoCuenta = tipo;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+       
+    public void retiro(int monto) {
+        this.saldo = saldo - monto;
+    }
+   
+    public void deposito(int monto) {
+        this.saldo = saldo + monto;
+    }    
+}
+
+
---
+++ Menu-del-banco/src/menu/del/banco/MenuDelBanco.java
@@ -0,0 +1,26 @@
+/*
+ *  Nombres                            carnet
+ *  
+    Emmanuel Antonio Zapata Batres    2000196
+ *  Andrea Iliana Cetino Ramírez      2000219
+ *  Josué Miguel Tejeda Solorzano     2000256
+ *  José Pablo Flores Herrera         2000104   Es el Lider y el que hizo el repositorio To change this license header, choose License Headers in Project Properties.
+ * 
+ */
+package menu.del.banco;
+
+/**
+ *
+ * @author Emmanuel Zapata
+ */
+public class MenuDelBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Menu menu = new Menu();
+        menu.menu();
+    }
+    
+}
